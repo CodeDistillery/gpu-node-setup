d949c333bd6f47ad449c4022c81a14ade9ebd053
latenssi <latenssi@users.noreply.github.com>
2018-12-11 09:42:26 +0200

    Update setup.sh

--- prometheus/setup.sh
+++ prometheus/setup.sh
@@ -3,4 +3,5 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${__dir}/run-nvidia-dcgm-exporter.sh \
-&& source ${__dir}/run-node-exporter.sh
+&& source ${__dir}/run-node-exporter.sh \
+&& source ${__dir}/run-cadvisor.sh
