70b28dba1204342e7f4bce1c2016359baebc5181
latenssi <latenssi@users.noreply.github.com>
2018-12-11 09:47:44 +0200

    Update run-nvidia-dcgm-exporter.sh

--- prometheus/run-nvidia-dcgm-exporter.sh
+++ prometheus/run-nvidia-dcgm-exporter.sh
@@ -2,7 +2,7 @@
 
 c_name=nvidia-dcgm-exporter
 
-docker stop ${c_name} && docker rm ${c_name};
+docker stop ${c_name} > /dev/null && docker rm ${c_name} > /dev/null;
 
 docker run \
   --name ${c_name} \
