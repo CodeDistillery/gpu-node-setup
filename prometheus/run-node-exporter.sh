d9f4a3c8b6c237064a2fe837c1c1a01d446cf2a1
latenssi <latenssi@users.noreply.github.com>
2018-12-11 08:43:04 +0200

    Update run-node-exporter.sh

--- prometheus/run-node-exporter.sh
+++ prometheus/run-node-exporter.sh
@@ -2,10 +2,10 @@
 docker run \
   --name node-exporter \
   --restart unless-stopped \
-  -d \
   --net="host" --pid="host" \
-  -v /:/host:ro,rslave \
   --volumes-from nvidia-dcgm-exporter:ro \
+  -v /:/host:ro,rslave \
+  -d \
   quay.io/prometheus/node-exporter \
      --path.rootfs /host \
      --collector.textfile.directory="/run/prometheus"
